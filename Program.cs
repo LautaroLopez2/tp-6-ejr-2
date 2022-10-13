886ddabe11b76680d30e5d4368e9e67bcfeefd0a
LautaroLopez2 <lautalopez2448@gmail.com>
2022-10-12 21:26:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tp_6_ejr_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string nombre;
+            Console.WriteLine("Escriba su nombre completo");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Bienvenido al ITES {0} que tengas un buen año", nombre);
+
+        }
+    }
+}
